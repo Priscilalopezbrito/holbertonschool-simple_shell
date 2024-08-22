f098c97891456603ce672f28255ef38475d7cae1
Heriberto Carreras <heriberto.carreras.devariel@gmail.com>
2024-08-22 15:22:35 -0400

    added tokenize function

--- shell_helper.c
+++ shell_helper.c
@@ -1,9 +1,10 @@
 #include "shell.h"
+
 /**
- * execute- executes a command using execve
- * @args: command
- * @prog_name: name
- **/
+* execute- executes a command using execve
+* @args: command
+* @prog_name: name
+**/
 void execute(char **args, char *prog_name)
 {
 	if (execve(args[0], args, environ) == -1)
@@ -19,14 +20,14 @@ void execute(char **args, char *prog_name)
 	}
 }
 
-
 /**
- * read_line- reads user input
- * Return: command
- **/
+* read_line- reads user input
+* Return: command
+**/
 char *read_line(void)
 {
 	char *command = NULL;
+
 	size_t len = 0;
 	ssize_t getinput;
 
@@ -50,10 +51,10 @@ char *read_line(void)
 }
 
 /**
- * fork_execute- fork process to execute command
- * @args: command
- * @prog_name: program name
- **/
+* fork_execute- fork process to execute command
+* @args: command
+* @prog_name: program name
+**/
 void fork_execute(char **args, char *prog_name)
 {
 	pid_t pid;
@@ -75,27 +76,19 @@ void fork_execute(char **args, char *prog_name)
 	}
 }
 
-
 /**
- * exec_commands- Tokenizes a command string into
- * individual arguments using spaces or newlines as delimiters
- * @command: command
- * @prog_name: name
- */
-
-void exec_commands(char *command, char *prog_name)
+* tokenize_command- Tokenizes a command string into
+* individual arguments using spaces or newlines as delimiters
+* @command: command
+* @args: array to store arguments
+* Return: number of arguments
+*/
+int tokenize_command(char *command, char **args)
 {
-	char *token, *args[100];
-	char *delim = "\n\t\r ";
-	int argc = 0;
-	char *path;
+	char *token, *delim = "\n\t\r ";
 
-	path = malloc(700 * sizeof(char));
-	if (path == NULL)
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
+	int argc = 0;
+
 	token = strtok(command, delim);
 	while (token != NULL && argc < 99)
 	{
@@ -106,6 +99,29 @@ void exec_commands(char *command, char *prog_name)
 		token = strtok(NULL, delim);
 	}
 	args[argc] = NULL;/*end of array*/
+	return (argc);
+}
+
+/**
+* exec_commands- executes a command
+* @command: command
+* @prog_name: name
+*/
+void exec_commands(char *command, char *prog_name)
+{
+	char *args[100];
+
+	char *path;
+
+	int argc;
+
+	path = malloc(700 * sizeof(char));
+	if (path == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	argc = tokenize_command(command, args);
 	if (args[0] != NULL && strcmp(args[0], "exit") == 0)/**/
 	{
 		free(command);
